4c1cbd82e169b215614de1ee67f7ccdbf77af04e
objektwerks <objektwerks@runbox.com>
2020-07-24 14:31:39 -0400

    implemented todo form

--- app/forms/TodoForm.scala
+++ app/forms/TodoForm.scala
@@ -1,3 +1,15 @@
 package forms
 
+import play.api.data.Form
+import play.api.data.Forms._
+
 final case class TodoForm(task: String, done: Boolean)
+
+object TodoForm {
+  val todoForm = Form(
+    mapping(
+      "task" -> nonEmptyText,
+      "done"  -> boolean
+    )(TodoForm.apply)(TodoForm.unapply)
+  )
+}
