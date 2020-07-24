727fa21ef03050c80d5556ea4a9ce33ce85cb0d1
objektwerks <objektwerks@runbox.com>
2020-07-24 14:13:37 -0400

    implemented todo repository; enhanced todo

--- app/repositories/TodoRepository.scala
+++ app/repositories/TodoRepository.scala
@@ -1,5 +1,11 @@
 package repositories
 
+import models.Todo
+
 object TodoRepository {
-  
+  private val todos = List.empty[Todo]
+
+  def add(todo: Todo): List[Todo] = todos :+ todo
+
+  def list(): List[Todo] = todos
 }
