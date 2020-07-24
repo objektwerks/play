b265b711d299b935ff108a54c967a05d956712f2
objektwerks <objektwerks@runbox.com>
2020-07-24 15:54:06 -0400

    delted todo repository, moving it into todo object

--- app/models/Todo.scala
+++ app/models/Todo.scala
@@ -6,10 +6,16 @@ object Todo {
   import play.api.data.Form
   import play.api.data.Forms._
 
+  private val todos = List.empty[Todo]
+
   val todoForm = Form(
     mapping(
       "task" -> nonEmptyText,
       "done"  -> boolean
     )(Todo.apply)(Todo.unapply)
   )
+
+  def add(todo: Todo): List[Todo] = todos :+ todo
+
+  def list(): List[Todo] = todos
 }
