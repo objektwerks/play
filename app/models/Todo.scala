a5a15cc298d4eca54c354ba662983771c5c1cbc3
objektwerks <objektwerks@runbox.com>
2020-07-24 15:42:01 -0400

    refactored todo form, again

--- app/models/Todo.scala
+++ app/models/Todo.scala
@@ -1,3 +1,15 @@
 package models
 
 final case class Todo(task: String, done: Boolean = false)
+
+object Todo {
+  import play.api.data.Form
+  import play.api.data.Forms._
+
+  val todoForm = Form(
+    mapping(
+      "task" -> nonEmptyText,
+      "done"  -> boolean
+    )(Todo.apply)(Todo.unapply)
+  )
+}
