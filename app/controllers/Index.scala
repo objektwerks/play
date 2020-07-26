219597a20b1b38cd5f2b4c485c4db89534f75242
objektwerks <objektwerks@runbox.com>
2020-07-26 18:13:27 -0400

    correct controller compile errors

--- app/controllers/Index.scala
+++ app/controllers/Index.scala
@@ -8,8 +8,9 @@ import play.api.mvc._
 import models._
 
 @Singleton
-class Index @Inject()(val controllerComponents: ControllerComponents) extends BaseController with Logging {
-  def index() = Action { implicit request: Request[AnyContent] =>
+class Index @Inject()(val messagesAction: MessagesActionBuilder,  val controllerComponents: ControllerComponents)
+  extends BaseController with Logging {
+  def index() = messagesAction { implicit request: MessagesRequest[AnyContent] =>
     logger.info(s"*** Index controller request: $request")
     Ok(views.html.index(Todo.todoForm, Todo.list))
   }
--- app/controllers/TodoController.scala
+++ app/controllers/TodoController.scala
@@ -5,12 +5,11 @@ import javax.inject.{Inject, Singleton}
 import play.api.Logging
 import play.api.mvc._
 
-import models._
-
 @Singleton
-class TodoController @Inject()(val controllerComponents: ControllerComponents) extends BaseController with Logging {
-  def add() = Action { implicit request: Request[AnyContent] =>
+class TodoController @Inject()(val messagesAction: MessagesActionBuilder,  val cc: MessagesControllerComponents)
+  extends MessagesAbstractController(cc) with Logging {
+  def add() = messagesAction { implicit request: MessagesRequest[AnyContent] =>
     logger.info(s"*** Todo controller request: $request")
-    Redirect(controllers.Index.index())
+    Redirect(routes.Index.index())
   }
 }
