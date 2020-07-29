6165659d217aee86a28be7fe882904f5cf109d09
objektwerks <objektwerks@runbox.com>
2020-07-29 15:38:55 -0400

    refactored tests; added timeout future test stub

---
+++ test/app/TimeoutFutureTest.scala
@@ -0,0 +1,9 @@
+package app
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class TimeoutFutureTest extends AnyFunSuite {
+  test("future") {
+    println("future test")
+  }
+}
---
+++ test/controllers/RoutesTest.scala
@@ -0,0 +1,19 @@
+package controllers
+
+import org.scalatestplus.play._
+import org.scalatestplus.play.guice._
+
+import play.api.test._
+import play.api.test.Helpers._
+
+class RoutesTest extends PlaySpec with GuiceOneAppPerTest {
+  "GET /" should {
+    "via routes" in {
+      val request = FakeRequest(GET, "/")
+      val home = route(app, request).get
+      status(home) mustBe OK
+      contentType(home) mustBe Some("text/html")
+      contentAsString(home) must include ("Todos")
+    }
+  }
+}
