6564a4ac9c92397cfd7a8bf32a193cf81ed73209
objektwerks <objektwerks@runbox.com>
2020-07-29 18:59:14 -0400

    enhanced future extensions test

--- test/test/FutureExtensionsTest.scala
+++ test/test/FutureExtensionsTest.scala
@@ -6,19 +6,22 @@ object FutureExtensions {
   import akka.actor.ActorSystem
   import akka.pattern.after
   import scala.concurrent.{ExecutionContext, Future}
+  import scala.concurrent._
   import scala.concurrent.duration._
 
   implicit class Extensions[T](future: Future[T]) {
     def withTimeout(futureFailed: => Throwable)
-                   (implicit timeout: FiniteDuration, system: ActorSystem, ec: ExecutionContext): Future[T] = {
-      Future.firstCompletedOf(Seq(future, after(timeout, system.scheduler)(Future.failed(futureFailed))))
+                   (implicit system: ActorSystem, ec: ExecutionContext, timeout: FiniteDuration): Future[T] = {
+      Future.firstCompletedOf(
+        Seq(
+          future,
+          after(timeout, system.scheduler)(Future.failed(futureFailed))
+        )
+      )
     }
   }
 
   def test(akkaTimeout: FiniteDuration, futureSleep: FiniteDuration): Future[Boolean] = {
-    import akka.actor.ActorSystem
-    import scala.concurrent._
-
     implicit val system = ActorSystem("future-extensions")
     implicit val dispatcher = system.dispatcher
     implicit val timeout = akkaTimeout
@@ -37,10 +40,17 @@ class FutureExtensionsTest extends AnyFunSuite {
   import scala.language.postfixOps
   import scala.util.{Failure, Success}
 
-  test("future with timeout") {
+  test("future with timeout > success") {
     FutureExtensions.test(akkaTimeout = 3 seconds, futureSleep = 2 seconds).onComplete {
       case Success(result) => assert(result)
       case Failure(_) => fail
+    }
+  }
+
+  test("future with timeout > failure") {
+    FutureExtensions.test(akkaTimeout = 2 seconds, futureSleep = 3 seconds).onComplete {
+      case Success(_) => fail
+      case Failure(error) => assert(error.getMessage.nonEmpty)
     }
   }
 }
