41be0ee748984bf30e7d3e0c45b0de8fa68a27a8
objektwerks <objektwerks@runbox.com>
2020-07-29 19:06:43 -0400

    extracted FutureExtensions from its test

---
+++ test/test/FutureExtensions.scala
@@ -0,0 +1,33 @@
+package test
+
+import akka.actor.ActorSystem
+import akka.pattern.after
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent._
+import scala.concurrent.duration._
+
+object FutureExtensions {
+  implicit class Extensions[T](future: Future[T]) {
+    def withTimeout(timeoutException: => Throwable)
+                   (implicit system: ActorSystem, ec: ExecutionContext, timeout: FiniteDuration): Future[T] = {
+      Future.firstCompletedOf(
+        Seq(
+          future,
+          after(timeout, system.scheduler)(Future.failed(timeoutException))
+        )
+      )
+    }
+  }
+
+  def test(akkaTimeout: FiniteDuration, futureSleep: FiniteDuration): Future[Boolean] = {
+    implicit val system = ActorSystem("future-extensions")
+    implicit val dispatcher = system.dispatcher
+    implicit val timeout = akkaTimeout
+
+    lazy val future = Future {
+      Thread.sleep(futureSleep.toMillis)
+      true
+    }
+    future.withTimeout(new TimeoutException("Future timed out!"))
+  }
+}
--- test/test/FutureExtensionsTest.scala
+++ test/test/FutureExtensionsTest.scala
@@ -2,38 +2,6 @@ package test
 
 import org.scalatest.funsuite.AnyFunSuite
 
-object FutureExtensions {
-  import akka.actor.ActorSystem
-  import akka.pattern.after
-  import scala.concurrent.{ExecutionContext, Future}
-  import scala.concurrent._
-  import scala.concurrent.duration._
-
-  implicit class Extensions[T](future: Future[T]) {
-    def withTimeout(futureFailed: => Throwable)
-                   (implicit system: ActorSystem, ec: ExecutionContext, timeout: FiniteDuration): Future[T] = {
-      Future.firstCompletedOf(
-        Seq(
-          future,
-          after(timeout, system.scheduler)(Future.failed(futureFailed))
-        )
-      )
-    }
-  }
-
-  def test(akkaTimeout: FiniteDuration, futureSleep: FiniteDuration): Future[Boolean] = {
-    implicit val system = ActorSystem("future-extensions")
-    implicit val dispatcher = system.dispatcher
-    implicit val timeout = akkaTimeout
-
-    lazy val future = Future {
-      Thread.sleep(futureSleep.toMillis)
-      true
-    }
-    future.withTimeout(new TimeoutException("Future timed out!"))
-  }
-}
-
 class FutureExtensionsTest extends AnyFunSuite {
   import scala.concurrent.ExecutionContext.Implicits.global
   import scala.concurrent.duration._
