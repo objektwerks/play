874ae73266bc00054da4b64d1fcb102685a2a8df
objektwerks <objektwerks@runbox.com>
2020-07-29 20:17:31 -0400

    enhanced future extensions test

--- test/test/FutureExtensionsTest.scala
+++ test/test/FutureExtensionsTest.scala
@@ -1,8 +1,12 @@
 package test
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
 
-class FutureExtensionsTest extends AnyFunSuite {
+import scala.annotation.tailrec
+import scala.concurrent.{Future, TimeoutException}
+
+class FutureExtensionsTest extends AnyFunSuite with Matchers {
   import scala.concurrent.ExecutionContext.Implicits.global
   import scala.concurrent.duration._
   import scala.language.postfixOps
@@ -10,7 +14,7 @@ class FutureExtensionsTest extends AnyFunSuite {
 
   test("future with timeout > success") {
     FutureExtensions.test(akkaTimeout = 3 seconds, futureSleep = 2 seconds).onComplete {
-      case Success(result) => assert(result)
+      case Success(result) => result shouldBe true
       case Failure(_) => fail
     }
   }
@@ -18,7 +22,30 @@ class FutureExtensionsTest extends AnyFunSuite {
   test("future with timeout > failure") {
     FutureExtensions.test(akkaTimeout = 2 seconds, futureSleep = 3 seconds).onComplete {
       case Success(_) => fail
-      case Failure(error) => assert(error.getMessage.nonEmpty)
+      case Failure(error) => error.getMessage.nonEmpty shouldBe true
     }
+  }
+
+  test("future factorial > success") {
+    import akka.actor.ActorSystem
+    import FutureExtensions._
+
+    implicit val system = ActorSystem("test")
+    implicit val dispatcher = system.dispatcher
+    implicit val timeout = 3 seconds
+
+    lazy val future = Future {
+      factorial(9)
+    }
+    future.withTimeout(new TimeoutException("Future timed out!")).onComplete {
+      case Success(result) => result shouldBe 362880
+      case Failure(_) => fail
+    }
+  }
+
+  @tailrec
+  private def factorial(n: Long, acc: Long = 1): Long = n match {
+    case i if i < 1 => acc
+    case _ => factorial(n - 1, acc * n)
   }
 }
